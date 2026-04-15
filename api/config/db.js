4880bce47a62f767f7577d6cc8f7c7b02d47dcab
LessandroFreitas <lessandrof@gmail.com>
2026-04-14 21:22:34 -0300

    retirado o DNS em api/config/db.js

--- api/config/db.js
+++ api/config/db.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const dns = require('dns');
-
-dns.setServers(['1.1.1.1', '1.0.0.1']);
 
 const dbUser     = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
